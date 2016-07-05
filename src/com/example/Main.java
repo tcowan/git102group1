9d8fe5eab6f48647c9381ca1e2a9c9bc0d181166
Ted Cowan <tcowan@my321.net>
2016-07-05 14:53:28 -0600

    Added the merge conflict println

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,6 +3,8 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Place the list of each object on the line below
+        System.out.println("Printing output from these objects:");
+
     }
 }

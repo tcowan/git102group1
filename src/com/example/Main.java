f9da3ea0b28f42823a93916346ac1987763580e3
Ted Cowan <tcowan@my321.net>
2016-07-05 15:56:46 -0600

    Added the Sample object

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,8 +3,11 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
-	// Place the list of each object on the line below
-        System.out.println("Printing output from these objects:");
+	    // Place the list of each object on the line below
+        System.out.println("Printing output from these objects: Sample");
 
+        // create a printing object for each group member and call its to_string() method
+        Sample sample = new Sample();
+        System.out.println(sample.toString());
     }
 }
---
+++ src/com/example/Sample.java
@@ -0,0 +1,15 @@
+package com.example;
+
+/**
+ * Created by tcowan on 7/5/16.
+ */
+public class Sample {
+        public Sample () {
+
+        }
+
+        public String toString() {
+            return "Hi, my name is Sample";
+        }
+
+}
